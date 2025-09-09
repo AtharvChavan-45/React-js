6a85e1af76a459f7a578d723b2e24804f645bf05
Atharv Ashvin Chavan <158123475+catharv-45@users.noreply.github.com>
2025-09-09 03:04:40 +0000

    basic react

--- 01basic-react/src/index.js
+++ 01basic-react/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+const anotheruser = 'Atharv'
+// create react element
+const reactElement =React.createElement(
+  'a',
+  {href: 'https://google.com',target:'_blank'},
+  'click me to visit google',
+  anotheruser // aotomaticaly enject from outside
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //<React.StrictMode>
+   //<App />
+  //</React.StrictMode>
+  //anotherElement
+  reactElement
 );
 
 
