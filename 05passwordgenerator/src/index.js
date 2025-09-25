6f513b1a40cd34c31f5578cbea59d00490c47077
Atharv Ashvin Chavan <158123475+catharv-45@users.noreply.github.com>
2025-09-25 15:38:56 +0000

    database connection file

--- 05passwordgenerator/src/index.js
+++ 05passwordgenerator/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import mongoose from "mongoose";
+import { DB_NAME } from './constants';
+
+//import express from "express"
+//const app = express()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +15,29 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+/*
+// approch one
+
+;(async() => {
+  try{
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on("error",(error)=>{
+      console.log("Error:", error);
+      throw error
+    })
+
+    app.listen(process.env.PORT,()=>{
+      console.log(`App is listening on port ${process.env.PORT}`);
+    })
+  }
+  catch(error){
+      console.error("ERROR",error)
+      throw err
+  }
+})()
+*/
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
