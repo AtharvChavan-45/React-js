68d8932d0c2a48acf6be4c48d2b165d2c4ba442f
Atharv Ashvin Chavan <158123475+catharv-45@users.noreply.github.com>
2025-09-26 05:03:49 +0000

    password generator

--- 05passwordgenerator/src/index.js
+++ 05passwordgenerator/src/index.js
@@ -1,19 +1,12 @@
-//method 1 : require('dotenv').config({path: './env'})
-//method 2:
-import dotenv from "dotenv"
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import connectDB from './db/index';
-//import express from "express"
-//const app = express()
-dotenv.config({
-  path:'./env'
-})
-connectDB()
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,27 +14,7 @@ root.render(
   </React.StrictMode>
 );
 
-/*
-// approch one
 
-;(async() => {
-  try{
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-    app.on("error",(error)=>{
-      console.log("Error:", error);
-      throw error
-    })
-
-    app.listen(process.env.PORT,()=>{
-      console.log(`App is listening on port ${process.env.PORT}`);
-    })
-  }
-  catch(error){
-      console.error("ERROR",error)
-      throw err
-  }
-})()
-*/
 
 
 // If you want to start measuring performance in your app, pass a function
