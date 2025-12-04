b218d2b9c86d927b40d38407f93e35fadc47f42d
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2025-12-04 04:44:53 +0000

    React router implementation

--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -6,6 +6,9 @@ import { RouterProvider ,createBrowserRouter} from 'react-router-dom'
 import About from './components/About/About.jsx'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 const router = createBrowserRouter([
   {
     path:'/',
@@ -17,10 +20,23 @@ const router = createBrowserRouter([
       },{
         path:'about',
         element:<About/>
+      },{
+        path:'contact',
+        element:<Contact/>
+      },{
+        path:'user',
+        element:<User/>
+      },{
+        
+        path:'github',
+        element:<Github/>
+
       }
     ]
   }
 ])
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>
