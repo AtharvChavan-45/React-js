b8a530f38ef3a6c2476cc6fb7ea7cf63d017bb18
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2025-12-07 11:44:44 +0000

    theme Switcher with the help of context API

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       <div className="w-full">
             <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                 {/* themeBtn*/}
-                <ThemeBtn/>
+                <ThemeBtn />
 
             </div>
 
