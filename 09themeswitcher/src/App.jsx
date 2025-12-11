7d4513d7334fc6322f5f03ee956021832ddf5201
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2025-12-11 05:31:57 +0000

    Theme switcher done

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
@@ -42,9 +42,7 @@ function App() {
                 {/* Card */}
                 <Card />
             </div>
-            <div>
-              hello
-            </div>
+           
       </div>
 </div>
 </ThemeProvider>
