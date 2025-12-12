5fa0d0f2ebd68eaf2688f88213412082a67823c9
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2025-12-12 06:41:43 +0000

    Redux todo list

--- 11reduxToolKitTodo/src/features/todo/todoSlice.js
+++ 11reduxToolKitTodo/src/features/todo/todoSlice.js
@@ -21,5 +21,18 @@ export const todoSlice =createSlice({
             state.todos = state.todos.filter((todo)=> 
                 todo.id !== action.payload)
         },
+        updateTodo: (state, action) => {
+        const { id, newText } = action.payload;
+
+        const todo = state.todos.find((todo) => todo.id === id);
+
+            if (todo) {
+            todo.text = newText;   // mutate directly because of Immer
+            }
+        }
     }
 })
+
+export const {addTodo,removeTodo,updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
