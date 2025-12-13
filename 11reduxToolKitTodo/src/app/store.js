b7c8fb7b6b694bb27b897bbcc69e9edd25f70b4d
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2025-12-13 13:39:51 +0000

    redux store

--- 11reduxToolKitTodo/src/app/store.js
+++ 11reduxToolKitTodo/src/app/store.js
@@ -1,6 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import todoReducer from '../features/todo/todoSlice';
 
+// store is like a global container for your app state
 export const store = configureStore({
     reducer:todoReducer
 })
+
+// configureStore is a function provided by redux toolkit
+// used to create the redux store
+// it is the modern and recommended way to create a store
+// it combines reducers ,  adds redux devtools support, adds middleware like redux-thunk
+
+//todoreducer is the default export from that slice
