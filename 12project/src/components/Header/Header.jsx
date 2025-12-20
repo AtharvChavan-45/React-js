78dd2b84cf43f5bdf5e67927c4d659956f7ec139
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2025-12-20 10:51:37 +0000

    Header with the help of useNavigate

--- 12project/src/components/Header/Header.jsx
+++ 12project/src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Container,Logo,LogoutBtn} from '../index'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'// used to read data from redux store
 import { useNavigate } from 'react-router-dom'
 
 function Header() {
-  const authStatus = useSelector((state)=>state.auth.Status)
+  const authStatus = useSelector((state)=>state.auth.Status) // chack status in authSlice is true or false
+  // stores the navigation function to redirect users using javascript
   const navigate = useNavigate()
 
+//An array that controls which menu items appear
   const navItems = [
     {
       name: 'Home',
@@ -17,7 +19,7 @@ function Header() {
     {
       name: "Login",
       slug: "/login",
-      active: !authStatus,
+      active: !authStatus,// login is shown when the user is not logged in
   },
   {
       name: "Signup",
@@ -45,19 +47,21 @@ function Header() {
                 </Link>
             </div>
             <ul className='flex ml-auto'>
-                {navItems.map((item)=>
+                {navItems.map((item)=> //loops through each navigation item
+                // item.active ? (...) : null it means show item only if active is true
                 item.active ? (
                   <li key={item.name}>
                     <button 
-                    onClick={()=>navigate(item.slug)}
-                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                    onClick={()=>navigate(item.slug)} // navigat to given slug address
+                    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
                     >
-                      {item.name}
+                      {/*print item name in header */}
+                      {item.name} 
                     </button>
                   </li>
                 ) : null
                 )}
-                {authStatus && (
+                {authStatus && ( // show logout button only if user is logged in
                   <li>
                     <LogoutBtn/>
                     </li>
