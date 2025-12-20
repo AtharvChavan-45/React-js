e25940d9484ada66f2a4f9af48ead4cf500e4eff
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2025-12-20 11:16:22 +0000

    reuseable components with the help of forwardRef hook

--- 12project/src/components/Input.jsx
+++ 12project/src/components/Input.jsx
@@ -1,5 +1,8 @@
 import React,{useId} from 'react'
+// forwardRef is a React function that allows a parent component
+//  to send a ref through a component to a child DOM element.
 
+// forwardRef forwards the ref from the parent to the actual HTML element.
 const Input = React.forwardRef(function Input({
     label,
     type = "text",
@@ -10,6 +13,9 @@ const Input = React.forwardRef(function Input({
     const id =useId()
     return(
         <div className='w-full'>
+            {/*It is conditional rendering 
+            means if label exists is true , render the <label>
+            if label is false, null, undefined or empty render nothing */}
             {label && <label className='inline-block mb-1 pl-1'
             htmlFor={id}>
                 {label}
@@ -19,7 +25,7 @@ const Input = React.forwardRef(function Input({
                 className={`px-3 py-2 rounded-lg bg-white
                     text-black outline-none focus:bg-gray-50
                     duration-200 border border-gray-200 w-full ${className}`}
-                    ref = {ref}
+                    ref = {ref} // provide reference in parent components
                     {...props}
                     id={id}
                  />
